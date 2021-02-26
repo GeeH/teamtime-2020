4319ec11fb9ac529bd1b6c64cf3ee8a0fe0dbab0
Skip <skip.barden@gmail.com>
2021-02-26 15:44:29 +0000

    implemented some new styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-wrap overflow-hidden p-4">
+    <div class="flex flex-wrap overflow-hidden md:px-6 pt-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xxl:grid-cols-5 col-gap-4">
         @foreach ($teams as $team)
 
-            <div class="w-64 p-2">
-                <h3 class=" pl-4 text-xl font-bold">{{ $team->name }}</h3>
+            <div class="">
+            <div class="bg-gray-700 rounded-md px-4 py-5 mb-8">
+                <h3 class="pl-1 text-lg font-bold text-white mb-5">{{ $team->name }}</h3>
 
             @foreach ($team->person as $person)
-                    <div class="w-full bg-white rounded-lg shadow-xl m-4 px-4">
-                        <div class="bg-white border-b border-gray-200">
-                            <div class="-ml-4 flex items-center justify-between flex-wrap sm:flex-no-wrap py-2">
-                                <div class="ml-4 mt-2">
-                                    <h3 class="text-lg font-medium text-gray-900">
+                    <div class="w-full bg-white rounded-md shadow-lg mb-3">
+                        <div class="bg-gray-200 border-b border-gray-200 rounded-t-lg">
+                            <div class="flex items-center justify-between flex-wrap sm:flex-no-wrap py-3half px-4">
+                                <div class="">
+                                    <h3 class="text-sm font-medium text-gray-900">
                                         {{ $person->name }}
                                     </h3>
                                 </div>
                             </div>
                         </div>
-                        <p class="mt-4 text-gray-900 person-time" data-timezone="{{ $person->timezone }}"></p>
+                        <p class="mt-4 px-4 text-gray-900 person-time text-sm" data-timezone="{{ $person->timezone }}"></p>
                         <span class="hidden" data-id="{{ $person->id }}"></span>
 
                         <div>
-                            <div class="ml-4 mt-2 text-right p-2 -mr-4">
+                            <div class="mt-1 text-right p-2">
                                 <a type="button" href="{{ route('edit-person', ['person' => $person]) }}"
                                    class="text-gray-500">
                                     <i class="fas fa-user-edit"></i>
@@ -35,6 +36,7 @@
                         </div>
                     </div>
                 @endforeach
+            </div>
             </div>
         @endforeach
     </div>
